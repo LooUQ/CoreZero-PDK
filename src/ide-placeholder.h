c121fd32b4966dc0964e2ae9c6901665292321f5
Greg Terrell <greg@loouq.com>
2019-09-23 11:22:06 -0400

    Create ide-placeholder.h
    Added empty .h file to suppress Arduino IDE warnings (invalid library, no headers found). This is a C++ library, so headers are .hpp

---
+++ src/ide-placeholder.h
@@ -0,0 +1,7 @@
+/* 
+* The CoreZero PDK is written in C++. Since many of the LooUQ/Circuit River libraries are mixed C/C++
+* we specify C++ headers with the convention of .hpp file extensions.
+*
+* The Arduino IDE will complain if a library contains no .h files. This empty .h file surpresses the warning.
+*/
+#define nothing 
